0922e83e80836dab03cf41a0c7657fde9580d338
Carla Campos <100176960+CarlagCampos@users.noreply.github.com>
2022-05-18 23:19:08 -0300

    Delete exercício2.js

--- variaveis/exercício2.js
+++
@@ -1,8 +0,0 @@
-let pergunta1,pergunta2,pergunta3
-pergunta1 = prompt("Está precisando de ajuda?")
-pergunta2 = prompt("Está estudando bastante?")
-pergunta3 = prompt("Está tendo dificuldade?")
-
-console.log("Está precisando de ajuda? - ",pergunta1)
-console.log("Está estudando bastante? - ",pergunta2)
-console.log("Está tendo dificuldade? - ",pergunta3)
