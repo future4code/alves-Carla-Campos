11ac78e0892fc58ffdd75f903ecc23717ed4723e
Carla_Campos <carla.g.campos@outlook.com>
2022-05-23 02:13:34 -0300

    Revisao javascript atualizada

--- revisao-postinho/index.js
+++ revisao-postinho/index.js
@@ -4,7 +4,7 @@
 
 // a-)Comparador de desigualdade a!==b 
 // Exemplo:
-
+/*
 function checarDesigualdade(a, b) {
 
     return `No comparador de desigualdade ${a}!==${b} é ${a !== b}`
@@ -14,57 +14,113 @@ console.log(checarDesigualdade(1, 3));
 // b-)Compare a igualdade entre a===b
 
 function checarIgualdade(a, b) {
-    //  Sua lógica aqui
-}
+        if (a === b){
+        console.log("Verdadeiro")
+        } else (console.log("Falso"));{
+
+        }
+
+} checarIgualdade(10, 10)
 
 // c-)Faça uma função chamada "verificaSeEMaior"
+function verificaSeEMaior( a, b) {
+    if (a > b ){
+    console.log("a maior")    
+    } else (console.log("b e maior"));{
 
-console.log(verificaSeEMaior(321, 2156));
+    }
+} verificaSeEMaior(23, 52)
+
+//console.log(verificaSeEMaior(321, 2156));
+
 
 /*
 // Exercício 2------------------------------------------------------------------------------------
 //Escreva true or false para as comparações abaixo:
 // exemplo 1>2 = false
-// a-) 1==='1'=
-// b-) 1=='1'=
-// c-) 'a'==='b'=
-// d-) 'b'>'a'=
-// e-) 0!==null=
-
+// a-) 1==='1'= // FALSE
+// b-) 1=='1'=// TRUE
+// c-) 'a'==='b'=// FALSE
+// d-) 'b'>'a'=// TRUE
+// e-) 0!==null=// TRUE
 
 // CONDICIONAIS
 
 // Exercício 3------------------------------------------------------------------------------------
 
-const cadastro = () => {
-    const usuario = []
 
-    //  Sua lógica aqui
+let usuario = []
+const cadastro = () => {
+     for (let i = 0; i < 1; i++){
+         usuario.push(prompt("qual o seu nome?"))
+         usuario.push(Number((prompt("qual o ano de seu nascimento?"))))
+         usuario.push(prompt("cadastre sua senha"))
+         usuario.push(prompt("qual sua nacionalidade?"))
+         if (2022 - usuario[1] < 18){
+             console.log("E preciso ter 18 anos ou mais para se cadastrar")
+         } else if ( usuario[2].length < 6){
+             console.log("A senha deve ter ao menos 6 caracteres")
+         };
+      console.log(usuario)   
+     }
 
 }
 console.log(cadastro());
-
+*/
 // Exercício 4-----------------------------------------------------------------------------------------------
 
-const login = () => {
-    const login = "labenu"
-    //  Sua lógica aqui
+//Escreva uma função que peça uma senha para o usuário e verifique se está é a mesma senha cadastrada por ele.  Dentro da função defina uma senha válida  Ex: const senha=”labenu”
 
-}
+//- Caso a senha seja válida imprima no console a mensagem (Usuário Logado)
+//- Caso a senha seja inválida imprima no console (Senha Inválida)
 
-console.log(login());
+/*
+function pedirSenha(){
+    const senhaOriginal = "Labenu"
+    let senhaUsuario = prompt("Digite a senha cadastrada")
+    if (senhaUsuario === senhaOriginal){
+        console.log("Usuário logado")
+    } else{
+        console.log("Senha inválida")
+    };
+} 
+pedirSenha()
 
+
+*/
+
+/*
 // Exercício 5----------------------------------------------------------------------------------------------------
 
 const primeiraDose = () => {
 
-    //  Sua lógica aqui
+    let nomeUsuario = prompt("Digite seu nome")
+    let tempo
+    let data
+    let vacina = prompt("Qual a vacina que tomou?")
+    if (vacina === "Coronavac"){
+       tempo = 28
+       data = "20/06/2022"
+       console.log(`Olá ${nomeUsuario}! A próxima dose da ${vacina} é daqui a ${tempo} dias. Compareça no posto na data ${data}.`)
 
+    } else if (vacina === "Astrazenica"){
+        tempo = 90
+        data = "23/07/2022"
+        console.log(`Olá ${nomeUsuario}! A próxima dose da ${vacina} é daqui a ${tempo} dias. Compareça no posto na data ${data}.`)
 
+    } else if (vacina === "Pfizer"){
+        tempo = 90
+        data = "23/07/2022"
+        console.log(`Olá ${nomeUsuario}! A próxima dose da ${vacina} é daqui a ${tempo} dias. Compareça no posto na data ${data}.`)    
+
+    } else {
+        console.log("Informação inválida")
+    };
 }
 console.log(primeiraDose())
 
-
+*/
+/*
 // LOOP+CONDICIONAL
 
 // Exercício 6 -------------------------------------------------------------------------------------
@@ -75,12 +131,15 @@ const segundaDose = (nomeDoUsuario) => {
         { nome: "Barbara", imunizacao: "incompleta" },
         { nome: "Carlos", imunizacao: "incompleta" }
     ]
-
-    //  Sua lógica aqui
+    let nomeUsuario = 
+    if (usuario.nome === "Artur"){
+        usuarios[0] = {nome: "Artur", imunizacao: "completa"}
+    }
+    
 
 
 }
-console.log(segundaDose("Barbara"));
+console.log(segundaDose("Artur"));
 
 // Exercício 7 --------------------------------------------------------------------------------------
 
