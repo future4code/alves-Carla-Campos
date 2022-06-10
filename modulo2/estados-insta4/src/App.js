2c2898dc36d10057d208306d87882a829b02c6d9
Carla Campos <100176960+CarlagCampos@users.noreply.github.com>
2022-06-10 00:04:24 -0300

    Update App.js
    Estou lançando manualmente pois estou com problemas ao dar push novamente.

--- modulo2/estados-insta4/src/App.js
+++ modulo2/estados-insta4/src/App.js
@@ -8,28 +8,121 @@ const MainContainer = styled.div`
   flex-direction: column;
   align-items: center;
 `
-
+const Formulario = styled.section`
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ margin-bottom: 3%;
+`
 class App extends React.Component {
+  state = {
+   pessoa: [
+     {
+      nomeUsuario: 'Carlinha',
+      fotoUsuario: 'https://t4.ftcdn.net/jpg/03/75/02/61/360_F_375026186_J7gHblTeVAIoKpJhRWX4VGlHcjDAQdFa.jpg',
+      fotoPost: 'https://i.pinimg.com/originals/72/05/99/720599c90ba6d33ee81297a67511655d.jpg',
+     },
+
+     {
+      nomeUsuario: 'Flavinha',
+      fotoUsuario: 'https://www.10wallpaper.com/wallpaper/2560x1600/1310/child_in_a_flower_field-cute_HD_wallpaper_2560x1600.jpg',
+      fotoPost: 'https://1.bp.blogspot.com/-3CSFD-be65k/XoUgkvOtSqI/AAAAAAAAqtQ/1QmUtClM4d0D2RFPcDn1hO_kIF89t5wTwCLcBGAsYHQ/s2560/field_flowers_sunset_drome_france_116125_1080x1920.jpg',
+     },
+
+     {
+      nomeUsuario: 'Lipe',
+      fotoUsuario: 'https://pbs.twimg.com/profile_images/1730575806/dia-do-rock-13-de-julho_400x400.jpg',
+      fotoPost: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToSoUcYB1XjZdhn9_abtLJ1cTZm2Tq6xRTpR0b2Cu8tNgUsopqsr_LCatPxU4QkZ_tjrE&usqp=CAU',
+     },
+   
+   
+   
+    ],
+
+    valorInputNome: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+
+
+}
+
+adicionaPost = () => {
+
+  const preparacaoDoPost = {
+    nomeUsuario: this.state.valorInputNome,
+
+    fotoUsuario: this.state.valorInputFotoUsuario,
+
+    fotoPost: this.state.valorInputFotoPost
+  }
+
+  const novoPost = [...this.state.pessoa, preparacaoDoPost]
+   
+  this.setState({
+    pessoa: novoPost,
+    valorInputNome: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+  })
+
+}
+  onChangeInputNome = (event) => {
+    this.setState({
+      valorInputNome: event.target.value
+    })
+  }
+
+  onChangeInputFotoUsuario = (event) => {
+    this.setState({
+      valorInputFotoUsuario: event.target.value
+    })
+  }
+
+  onChangeInputFotoPost = (event) => {
+    this.setState({
+      valorInputFotoPost: event.target.value
+    })
+  }
+
+
+
   render() {
+    const listaDePost = this.state.pessoa.map((post, indice) => {
+      return <Post key={indice}
+        nomeUsuario={post.nomeUsuario}
+        fotoUsuario={post.fotoUsuario}
+        fotoPost={post.fotoPost}/>   
+      
+    })
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'Carlinha'}
-          fotoUsuario={'https://t4.ftcdn.net/jpg/03/75/02/61/360_F_375026186_J7gHblTeVAIoKpJhRWX4VGlHcjDAQdFa.jpg'}
-          fotoPost={'https://i.pinimg.com/originals/72/05/99/720599c90ba6d33ee81297a67511655d.jpg'}
-        />
-        
-        <Post
-          nomeUsuario={'Flavinha'}
-          fotoUsuario={'https://www.10wallpaper.com/wallpaper/2560x1600/1310/child_in_a_flower_field-cute_HD_wallpaper_2560x1600.jpg'}
-          fotoPost={'https://1.bp.blogspot.com/-3CSFD-be65k/XoUgkvOtSqI/AAAAAAAAqtQ/1QmUtClM4d0D2RFPcDn1hO_kIF89t5wTwCLcBGAsYHQ/s2560/field_flowers_sunset_drome_france_116125_1080x1920.jpg'}
-        />
 
-        <Post
-          nomeUsuario={'Lipe'}
-          fotoUsuario={'https://pbs.twimg.com/profile_images/1730575806/dia-do-rock-13-de-julho_400x400.jpg'}
-          fotoPost={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToSoUcYB1XjZdhn9_abtLJ1cTZm2Tq6xRTpR0b2Cu8tNgUsopqsr_LCatPxU4QkZ_tjrE&usqp=CAU'}
-        />
+        <h1> Criação de Post</h1>
+       
+       <Formulario>
+         
+
+         <input
+         value= {this.state.valorInputNome}
+         onChange={this.onChangeInputNome}
+         placeholder={"Nome Usuario"} />
+
+        <input
+         value= {this.state.valorInputFotoUsuario}
+         onChange={this.onChangeInputFotoUsuario}
+         placeholder={"Link Foto Usuário"} />
+
+        <input
+         value= {this.state.valorInputFotoPost}
+         onChange={this.onChangeInputFotoPost}
+         placeholder={"Link Foto do Post"} />
+
+         <button onClick={this.adicionaPost}>Adicionar</button>
+                  
+        </Formulario>
+             
+        {listaDePost}
+
       </MainContainer>
     );
   }
